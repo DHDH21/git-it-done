7154dc5ac2bb5696a7454d1f4653f01b307f5ec5
DHDH <damon.ha6@gmail.com>
2021-07-23 12:04:46 -0400

    updated homepagejs

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,10 +1,16 @@
 //js starts
 
-fetch("https://api.github.com/users/octocat/repos").then(function(response) {
-  response.json().then(function(data) {
-    console.log(data);
-  });
-});
 
-console.log("outside");
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
+  };
 
+  getUserRepos("dhdh");
